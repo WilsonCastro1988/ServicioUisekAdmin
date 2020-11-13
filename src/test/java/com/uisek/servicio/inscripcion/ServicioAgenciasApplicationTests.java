bc9cce8dcb768b0aec701e63332874f2e39a6b59
WillysKapitalink <wilson.castro@kpitalink.com>
2020-11-13 09:37:31 -0500

    carga inicial

---
+++ src/test/java/com/uisek/servicio/inscripcion/ServicioAgenciasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uisek.servicio.inscripcion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServicioAgenciasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
